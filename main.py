e38c066c54a803fb49b8092fb37ca8cd0e4f649b
RiedelNicolas <nariedel99@gmail.com>
2024-10-12 20:12:21 -0300

    reconstruir camino

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ def _juego_monedas(arr, inicio = 0, fin = 0, memory = {}):
 
     return ganancia
 
-# Dado un arreglo de monedas, devuelve la ganancia maxima de sofia
+# Dado un arreglo de monedas, devuelve la ganancia maxima de Sofia y la memoria de las llamadas recursivas.
 def juego_monedas(arr):
     memory = {}
-    return _juego_monedas(arr, 0, len(arr), memory)
+    return _juego_monedas(arr, 0, len(arr), memory), memory
 
 # Dado un archivo csv, devuelve una lista con las monedas
 def obtener_monedas(path):
@@ -43,12 +43,56 @@ def obtener_monedas(path):
         csv_reader = csv.reader(f, delimiter=";")
         return [int(moneda) for moneda in next(csv_reader)]
 
+# Dado la memoria de llamadas recursivas y el arreglo de monedas, reconstruye el camino de las decisiones tomadas.
+def reconstruir_solucion(memory, arr):
+    solucion = []
+    inicio = 0
+    fin = len(arr)
+    ganancia_mateo = 0
+
+    def calcular_decision_mateo(inicio, fin):
+        # Caso base
+        if inicio >= fin:
+            return 0
+        if arr[inicio] > arr[fin - 1]:
+            solucion.append(f'Mateo agarra la primera ({arr[inicio]})')
+            return arr[inicio]
+        else:
+            solucion.append(f'Mateo agarra la ultima ({arr[fin-1]})')
+            return arr[fin-1]
+
+    while inicio < fin:
+        decision_mateo_1 = decision_mateo(arr, inicio + 1, fin) # En este caso, Mate agarro la primera
+        decision_mateo_2 = decision_mateo(arr, inicio, fin-1) # En este caso, Mate agarro la ultima
+
+        if memory[(inicio, fin)] == arr[inicio] + memory.get((decision_mateo_1[0], decision_mateo_1[1]), 0):
+            solucion.append(f'Sophia debe agarrar la primera ({arr[inicio]})')
+            ganancia_mateo += calcular_decision_mateo(inicio+1, fin)
+            inicio = decision_mateo_1[0]
+            fin = decision_mateo_1[1]
+        else:
+            solucion.append(f'Sophia debe agarrar la ultima ({arr[fin-1]})')
+            ganancia_mateo += calcular_decision_mateo(inicio, fin-1)
+            inicio = decision_mateo_2[0]
+            fin = decision_mateo_2[1]
+
+    return solucion, ganancia_mateo
+
 def main():
-    ejemplos = [f for f in os.listdir("ejemplos") if f.endswith(".txt")]
+
+    monedas = obtener_monedas("ejemplos/5.txt")
+    resultado, memoria = juego_monedas(monedas)
+
+    print(f"El resultado para 5.txt es: {resultado}")
+
+    solucion, ganancia_mateo = reconstruir_solucion(memoria, monedas)
+    print("La solución es:")
+    for elemento in solucion:
+        print(elemento)
     
-    for ejemplo in sorted (ejemplos, key = lambda x: int(x.split(".")[0])):
-        monedas = obtener_monedas(f"ejemplos/{ejemplo}")
-        print(f"El resultado para {ejemplo} es: {juego_monedas(monedas)}")
+    print(f"La ganancia de Mateo es: {ganancia_mateo}")
+    print(f"La ganancia de Sofia es: {resultado}")
+
 
 if __name__ == "__main__":
     main()
